25f4721ec6e6e7b47ca108175273996747773511
JaegyuJeong <h923313@gmail.com>
2020-05-24 19:40:37 +0900

    iterable, iterator protocol:

---
+++ loop.js
@@ -0,0 +1,24 @@
+const log = console.log;
+log('Arr ==============');
+const arr = [1,2,3];
+// arr[Symbol.iterator] = null;
+let iter1 = arr[Symbol.iterator]();
+iter1.next();
+iter1.next();
+for(const a of iter1) log(a);
+
+
+log('Set ==============');
+const set = new Set([1,2,3]);
+for (const a of set) log(a);
+
+log('Map ==============');
+const map = new Map([['a',1], ['b',2], ['c',3]]);
+let iter2 = map[Symbol.iterator]();
+iter2.next();
+for(const a of iter2 )log(a);
+
+// ## 이터러블/이터레이터 프로토콜
+//  - 이터러블 : 이터레이터를 리턴하는 [Symbol.iterator]() 를 가진 값
+//  - 이터레이터 : { value, done } 객체를 리턴하는 next() 를 가진 값
+//  - 이러러블/이터레이터 프로토콜 : 이터러블을 for...of, 전개 연산자 등과 함께 동작하도록한 규

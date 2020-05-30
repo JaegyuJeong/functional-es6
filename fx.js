a26ccb444116ca168b737739dcf0e1fc93f7a5d6
JaegyuJeong <h923313@gmail.com>
2020-05-30 23:47:59 +0900

    shopping bucket

--- fx.js
+++ fx.js
@@ -30,25 +30,25 @@ const reduce = curry((f, acc, iter) => {
     return acc;
 })
 
-
-const products = [
-    {name: '반팔티', price: 15000},
-    {name: '긴팔티', price: 20000},
-    {name: '핸드폰케이스', price: 15000},
-    {name: '후드티', price: 30000},
-    {name: '바지', price: 25000},
-]
+//
+// const products = [
+//     {name: '반팔티', price: 15000},
+//     {name: '긴팔티', price: 20000},
+//     {name: '핸드폰케이스', price: 15000},
+//     {name: '후드티', price: 30000},
+//     {name: '바지', price: 25000},
+// ]
 
 const add = (a, b) => a + b;
-log(
-    reduce(
-        add,
-        map(p => p.price, filter(p => p.price < 20000, products)))
-)
-
-log(reduce(
-    add,
-    filter(p => p >= 20000, map(p => p.price, products))))
+// log(
+//     reduce(
+//         add,
+//         map(p => p.price, filter(p => p.price < 20000, products)))
+// )
+//
+// log(reduce(
+//     add,
+//     filter(p => p >= 20000, map(p => p.price, products))))
 
 // # go, pipe
 
@@ -70,34 +70,14 @@ const f = pipe(
 
 log(f(0, 1))
 
+//
+// go(products,
+//     products => filter(p => p.price < 20000, products),
+//     products => map(p => p.price, products),
+//     prices => reduce(add, prices),
+//     log
+// );
 
-go(products,
-    products => filter(p => p.price < 20000, products),
-    products => map(p => p.price, products),
-    prices => reduce(add, prices),
-    log
-);
-
-const total_price = pipe(
-    map(p => p.price),
-    reduce(add)
-)
-
-const base_total_price = predi => pipe(
-    filter(predi),
-    total_price);
-)
-
-go(
-    products,
-    base_total_price(p => p.price < 20000),
-    log
-);
-
-go(
-    products,
-    base_total_price(p => p.price >= 20000),
-    log)
 
 // ## curry ==> 받은함수를 원하는 시점에 평가하는 함수
 
